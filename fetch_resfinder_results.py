75fe3ab316add009e2e89d41b3282fd136bc7967
profan <robinhubner@gmail.com>
2022-04-09 15:14:51 +0100

    remove some unused imports.

--- fetch_resfinder_results.py
+++ fetch_resfinder_results.py
@@ -1,23 +1,14 @@
-from time import sleep
-from typing import Optional
-
 from rich.progress import track
+from time import sleep
 import argparse
-import csv
 import os
 
 from selenium import webdriver
-
-from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.remote.webdriver import WebDriver
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.ui import Select
 
 # our utils
 import util
